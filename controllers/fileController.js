b4ac7a18bde574062dbf43949e363c6e8f087f7f
Imadnan23 <adnanadil93045@gmail.com>
2026-06-13 23:43:24 +0530

    store metadata in Mongodb

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,11 +1,67 @@
-const uploadFile = (req,res)=>{
-    console.log(req.file);
-    res.json({
+const File = require("../models/File");
+
+
+
+const uploadFile = async(req,res)=>{
+
+
+    if(!req.file){
+
+
+        return res.status(400).json({
+
+            success:false,
+            message:"No file uploaded"
+
+        });
+
+
+    }
+
+
+
+    const savedFile = await File.create({
+
+
+        filename:req.file.filename,
+
+
+        originalName:req.file.originalname,
+
+
+        path:req.file.path,
+
+
+        mimetype:req.file.mimetype,
+
+
+        size:req.file.size
+
+
+    });
+
+
+
+    res.status(201).json({
+
+
+        success:true,
+
+
         message:"File uploaded successfully",
-        file:req.file
-    })
-}
+
+
+        file:savedFile
+
+
+    });
+
+
+
+};
+
+
 
 module.exports={
     uploadFile
-}
+};
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 require("dotenv").config();
+const connectDB = require("./config/db");
+connectDB();
 
 const fileRoutes = require("./routes/fileRoutes");
 const errorHandler = require("./middleware/errorMiddleware");
